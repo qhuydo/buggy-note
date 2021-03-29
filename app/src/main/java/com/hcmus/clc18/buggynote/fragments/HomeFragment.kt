787febf4648845442318d74af1abf2226e46a6fe
qhuydo <q.huy.do.172@gmail.com>
2021-03-29 19:51:36 +0700

    Initialize the project
    [+] *: create the project from the project template with several
           modification in order to reduce the redundant code.
           Nothing special tbh.

---
+++ app/src/main/java/com/hcmus/clc18/buggynote/fragments/HomeFragment.kt
@@ -0,0 +1,19 @@
+package com.hcmus.clc18.buggynote.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.hcmus.clc18.buggynote.R
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
